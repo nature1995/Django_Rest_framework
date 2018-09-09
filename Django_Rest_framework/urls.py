6028ec397db6d8f1fd2310a94f0e2dd1657b1da0
Unknown <gzr2008@vip.qq.com>
2018-09-09 14:49:59 -0400

    Update 20180912
    Update 20180912

---
+++ Django_Rest_framework/urls.py
@@ -0,0 +1,38 @@
+"""Django_Rest_framework URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.conf.urls import url, include
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from musics.views import MusicViewSet
+from shares.views import ShareViewSet
+from django.urls import path
+from . import views
+from tools.views import generate_qrcode
+
+
+router = DefaultRouter()
+router.register(r'music', MusicViewSet, base_name='music')
+router.register(r'shares', ShareViewSet, base_name='share')
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^qr/(.+)$',generate_qrcode , name='qr'),
+    path(r'', views.home, name='主页'),
+
+]
